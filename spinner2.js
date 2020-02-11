40ed934ac222ed0cd087bcd7d85776bf599e4be8
Hatem <hatem.deux@gmail.com>
2020-02-11 15:37:18 -0500

    spinner 2 added, refactoring spinner 1 - collaboration : Victor & Hatem

---
+++ spinner2.js
@@ -0,0 +1,19 @@
+const spin = function (spinCount) {
+  const spinner = ['|', '/', '-', '\\']
+  // const initialDelay = 100;
+  const step = 200;
+  let time = 100; //initial time wait
+  process.stdout.write('\n');
+  for (let spinNum = 0; spinNum < spinCount; spinNum++) {
+    for (let spinIndex in spinner) {
+      setTimeout(() => {
+        // console.log(time)
+        process.stdout.write(`\r${spinner[spinIndex]}   `);
+      }, time);
+      time += step;
+      
+    }
+  } 
+  setTimeout(() => process.stdout.write('\n'), time);
+}
+spin(3);
